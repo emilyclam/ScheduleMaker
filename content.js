f9e7ffab9169cd9e21d8d740849ad562a2fde960
emilyclam <76188564+emilyclam@users.noreply.github.com>
2021-10-23 16:38:32 -0700

    the alarm works!
    i know there aren't many changes in this pull, but i just wanted to save this

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-const sound = new Audio("bell.wav")
+const sound = new Audio(chrome.runtime.getURL('bell.wav')) 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.sound == "on") {
-        alert("gogogo")
+        sound.play()
     }
 })
